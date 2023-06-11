301a16098f946e5f78306fc43fd7a01ebbe44c4c
refrantz <renan262001@gmail.com>
2023-06-11 18:31:24 -0300

    todo - test with clumsy

--- UDPCLient.java
+++ UDPCLient.java
@@ -3,15 +3,95 @@
 
 import java.io.*; // classes para input e output streams e
 import java.net.*;// DatagramaSocket,InetAddress,DatagramaPacket
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.zip.CRC32;
 
 class UDPClient {
 
-   public static void sendPackets(){
-      
-   }
-
    public static void main(String args[]) throws Exception
    {
+
+      //declare globals
+
+
+      int chunkSize = 300;
+      int paddingMetaSize = 4;
+      int checkSumSize = 20;
+      int seqNumSize = 10;
+
+      byte[] sendData = new byte[chunkSize];
+      byte[] receiveData = new byte[chunkSize];
+      int acresc = 1;
+      boolean AllACK = true;
+      int congestWindow = 20;
+      int retransmitCounter = 0;
+
+
+
+      // Load the file into chunks and append metadata
+
+      Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+      System.out.println("Insira o nome do arquivo: ");
+
+      String fileName = scanner.nextLine();  // Read user input
+      File file = new File(fileName);  // Use the input to create a File object
+
+      scanner.close();  // Don't forget to close the scanner
+
+      FileInputStream fis = new FileInputStream(file);
+
+      // Buffer to hold each chunk
+      byte[] buffer = new byte[chunkSize-checkSumSize-seqNumSize-paddingMetaSize]; // 270 bytes for data, 20 bytes for checksum, 10 bytes for sequence number, 4 bytes to specify padding
+
+      // Read the file chunk by chunk
+      List<byte[]> packets = new ArrayList<>();
+      int bytesRead;
+      int sequenceNumber = 0;
+      int padding = 0;
+      while ((bytesRead = fis.read(buffer)) != -1) {
+         // At this point, 'buffer' contains a chunk of the file,
+         // and 'bytesRead' is the number of bytes read into the buffer.
+
+         //data is smaller than chunk size
+         if(bytesRead < buffer.length){
+            padding = buffer.length - bytesRead;
+         }
+
+         // If the last chunk was smaller than the chunk size, adjust the buffer size accordingly.
+
+         // Calculate the checksum
+         CRC32 crc = new CRC32();
+         crc.update(buffer);
+         long checksum = crc.getValue();
+
+         System.out.println(checksum);
+
+         // Append the checksum and sequence number to the chunk
+         String metadata = String.format("%4d%10d%20d", padding, sequenceNumber, checksum);
+         byte[] metadataBytes = metadata.getBytes();
+         byte[] packet = new byte[chunkSize];
+         System.arraycopy(buffer, 0, packet, 0, buffer.length);
+         System.arraycopy(metadataBytes, 0, packet, buffer.length, metadataBytes.length);
+         packets.add(packet);
+         sequenceNumber++;
+         buffer = new byte[chunkSize-checkSumSize-seqNumSize-paddingMetaSize];
+      }
+
+      int totalPackets = (int) Math.ceil((double) file.length() / (chunkSize - checkSumSize - seqNumSize - paddingMetaSize));
+      int sentPackets = 0;
+
+      fis.close();
+
+
+      //
+
+
+      //Send initial connection
+
+
       // declara socket cliente
       DatagramSocket clientSocket = new DatagramSocket();
       clientSocket.setSoTimeout(1000);
@@ -19,22 +99,44 @@ class UDPClient {
       // obtem endere�o IP do servidor com o DNS
       InetAddress IPAddress = InetAddress.getByName("localhost");
 
-      byte[] sendData = new byte[1024];
-      byte[] receiveData = new byte[1024];
-      int acresc = 1;
-      int totalPackets = 10;
-      int sentPackets = 0;
-      boolean AllACK = true;
-      int congestWindow = 20;
-      int retransmitCounter = 0;
+      sendData = ("Connect with size:" + totalPackets).getBytes();
+
+      DatagramPacket initialPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+      clientSocket.send(initialPacket);
+
+      DatagramPacket receiveInitialPacket = new DatagramPacket(receiveData, receiveData.length);
+      
+      try {
+         clientSocket.receive(receiveInitialPacket);
+         String initialConfirm = new String(receiveInitialPacket.getData()).trim();
+
+         if (initialConfirm.equals("Confirm connection")){
+            System.out.println("Connection succeded");
+         }else{
+            System.out.println("Connection failed");
+         }
+         
+      } catch (SocketTimeoutException e) {
+         System.out.println("Connection failed");
+      }
+
+
+      //
+
+
+
+      //send packets
+
+      sendData = new byte[chunkSize];
+      receiveData = new byte[chunkSize];
 
       while (sentPackets < totalPackets){
 
          for (int i = 0; i < Math.min(acresc, totalPackets-sentPackets); i++){
-            // l� uma linha do teclado
-            int packNumber = sentPackets+i;
-            String sentence = "gremio"+"|seq:"+packNumber;
-            sendData = sentence.getBytes();
+
+            int packNumber = sentPackets+i; 
+            sendData = packets.get(packNumber);
       
             // cria pacote com o dado, o endere�o do server e porta do servidor
             DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
@@ -54,6 +156,7 @@ class UDPClient {
               break;
            }
       
+           //
             String sentenceACK = new String(receivePacket.getData());
             int ACKSeqNum = Integer.parseInt(sentenceACK.split(":")[1].trim());
             System.out.println(sentenceACK); 
@@ -67,9 +170,8 @@ class UDPClient {
                retransmitCounter++;
 
                if (retransmitCounter == 3) {
-                  // retransmit packet lastAckedPacket + 1
-                  String sentence = "gremio" + "|seq:" + sentPackets;
-                  sendData = sentence.getBytes();
+
+                  sendData = packets.get(sentPackets);
                   DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
                   clientSocket.send(sendPacket);
                
@@ -82,7 +184,12 @@ class UDPClient {
             }else{
                retransmitCounter=0;
                sentPackets++;
-               if (sentPackets >= totalPackets) {break;}
+               if (sentPackets >= totalPackets) {
+                  sendData = "Terminate connection".getBytes();
+                  DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+                  clientSocket.send(sendPacket);
+                  break;
+               }
             }
 
          }
@@ -98,6 +205,7 @@ class UDPClient {
       }
 
       // fecha o cliente
+      System.out.println("Connection Terminated");
       clientSocket.close();
    }
 }
--- UDPServer.java
+++ UDPServer.java
@@ -2,39 +2,122 @@
 // Separa o dado, o endere�o IP e a porta deste cliente
 // Imprime o dado na tela
 
-import java.io.*;
+import java.io.FileOutputStream;
 import java.net.*;
+import java.util.Arrays;
+import java.util.zip.CRC32;
 
 class UDPServer {
-   public static void main(String args[])  throws Exception
-      {
-         // cria socket do servidor com a porta 9876
-         DatagramSocket serverSocket = new DatagramSocket(9876);
+   public static void main(String args[])  throws Exception{
 
-            byte[] receiveData = new byte[1024];
-            byte[] sendData = new byte[1024];
+      // cria socket do servidor com a porta 9876
+
+      int chunkSize = 300;
+      int chunkDataSize = 266;
+      byte[] receiveData = new byte[chunkSize];
+      byte[] sendData = new byte[chunkSize];
+
+      DatagramSocket serverSocket = new DatagramSocket(9876);
+
+      DatagramPacket initialPacket = new DatagramPacket(receiveData, receiveData.length);
+      serverSocket.receive(initialPacket);
+
+      InetAddress IPAddress = initialPacket.getAddress();
+      int port = initialPacket.getPort();
+
+      int totalPackets = Integer.parseInt(new String(initialPacket.getData()).trim().split(":")[1]);
+      Boolean[] packetReceived = new Boolean[totalPackets];
+      Arrays.fill(packetReceived, Boolean.FALSE);
+
+      sendData = "Confirm connection".getBytes();
+      DatagramPacket sendInitialPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+      serverSocket.send(sendInitialPacket);
+
+
+      //
+
+
+      sendData = new byte[chunkSize];
+      receiveData = new byte[chunkSize];
+
+      int lastACKNum = 0;
+
+      FileOutputStream fos = new FileOutputStream("received_gremio.png");
+
+      while(Arrays.asList(packetReceived).contains(false)){
+            // declara o pacote a ser recebido
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+            // recebe o pacote do cliente
+            serverSocket.receive(receivePacket);
+
+            String sentence = new String(receivePacket.getData());
+            if(sentence.trim().equals("Terminate connection")){
+               break;
+            }
+
+            byte[] sentenceBytes = receivePacket.getData();
+
+            // Extract the last 20 characters for the checksum
+            byte[] checksumBytes = Arrays.copyOfRange(sentenceBytes, sentenceBytes.length - 20, sentenceBytes.length);
+            String checksumStr = new String(checksumBytes);
+            long checksum = Long.parseLong(checksumStr.trim());
             
-            while(true)
-               {
-                  // declara o pacote a ser recebido
-                  DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 
-                  // recebe o pacote do cliente
-                  serverSocket.receive(receivePacket);
+            // Extract the 10 characters before the checksum for the sequence number
+            byte[] seqNumBytes = Arrays.copyOfRange(sentenceBytes, sentenceBytes.length - 30, sentenceBytes.length - 20);
+            String seqNumStr = new String(seqNumBytes);
+            int seqNum = Integer.parseInt(seqNumStr.trim());
 
-                  // pega os dados, o endere�o IP e a porta do cliente
-                  // para poder mandar a msg de volta
-                  String sentence = new String(receivePacket.getData());
-                  InetAddress IPAddress = receivePacket.getAddress();
-                  int port = receivePacket.getPort();
-                  int seqNum = Integer.parseInt(sentence.split(":")[1].trim())+1;
-                  String sentenceACK = "ACK|seq:"+seqNum;
-                  sendData = sentenceACK.getBytes();
+            System.out.println(seqNum);
 
-                  DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
-                  serverSocket.send(sendPacket);
+            // Extract the 4 characters before the seqnum for the padding size
+            byte[] paddingSizeBytes = Arrays.copyOfRange(sentenceBytes, sentenceBytes.length - 34, sentenceBytes.length - 30);
+            String paddingSizeStr = new String(paddingSizeBytes);
+            int paddingSize = Integer.parseInt(paddingSizeStr.trim());
 
-                  System.out.println("Mensagem recebida: " + sentence);
-               }
+            System.out.println(paddingSize);
+
+            // Calculate the checksum
+            CRC32 crc = new CRC32();
+            crc.update(sentenceBytes, 0, chunkDataSize); // Only calculate checksum for the data part
+            long calculatedChecksum = crc.getValue();
+
+            System.out.println(checksum);
+            System.out.println(calculatedChecksum);
+
+            
+            //for(byte b : sentenceBytes){
+            //   System.out.println(b);
+            //}
+
+            
+            if((seqNum == 0 || packetReceived[seqNum-1]) && (checksum == calculatedChecksum)){
+
+               fos.write(sentenceBytes, 0, chunkDataSize-paddingSize); // Only write the data part
+
+               packetReceived[seqNum] = true;
+   
+               String sentenceACK = "ACK|seq:"+(seqNum+1);
+               sendData = sentenceACK.getBytes();
+               DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+               serverSocket.send(sendPacket);
+               lastACKNum = seqNum+1;
+
+            }else{
+
+               String sentenceACK = "ACK|seq:"+(lastACKNum);
+               sendData = sentenceACK.getBytes();
+               DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+               serverSocket.send(sendPacket);
+
+            }
+
+            //System.out.println("Mensagem recebida: " + sentence);
       }
+
+      System.out.println("Connection terminated");
+      serverSocket.close();
+
+   }
 }
